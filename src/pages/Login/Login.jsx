570a7ef32051f9a0f6d7026596785d280007063e
gaeunchoi <gaeun1884@naver.com>
2025-04-01 19:42:21 +0900

    Feat: Login 화면 구성

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+const Login = () => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const navigate = useNavigate();
+
+  const handleIdChanged = (e) => {
+    setId(e.target.value);
+  };
+
+  const handlePwChanged = (e) => {
+    setPw(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("ID: ", id, "PW:", pw);
+
+    if (id === "admin@test.com" && pw === "admin") {
+      navigate("/chatlist");
+    } else {
+      alert("로그인 정보가 올바르지 않습니다");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <img src="../src/assets/kakaotalk-logo.png" className="logo" />
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="아이디(E-mail)"
+          value={id}
+          onChange={handleIdChanged}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={pw}
+          onChange={handlePwChanged}
+        />
+        <button type="submit">로그인</button>
+        <p onClick={() => navigate("./Signup")}>이메일로 회원가입</p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
