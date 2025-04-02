d7edb1e88c424dec40d485f4c9c99de5fb25a5c4
gaeunchoi <gaeun1884@naver.com>
2025-04-02 21:22:10 +0900

    Feat: ID 이메일 포맷 체크, 로그인 유효성 검사, 에러메시지 출력

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/kakaotalk-logo.png";
+import { isValidEmail } from "../../utils/emailValidation";
 import "./Login.css";
 
 const Login = () => {
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
+  const [errorMesasge, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const handleIdChanged = (e) => {
     setId(e.target.value);
+
+    // ID 이메일 형식 체크
+    if (!isValidEmail(id)) {
+      setErrorMessage("아이디는 이메일 형식으로 입력해야합니다.");
+      return;
+    }
+    setErrorMessage("");
   };
 
   const handlePwChanged = (e) => {
@@ -18,14 +27,16 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("ID: ", id, "PW:", pw);
 
     if (id === "admin@test.com" && pw === "admin") {
       navigate("/chatlist");
     } else {
-      alert("로그인 정보가 올바르지 않습니다");
+      setErrorMessage("로그인 정보가 올바르지 않습니다");
+      return;
     }
   };
+
+  const isBtnDisabled = id === "" || pw === "" || !isValidEmail(id);
 
   return (
     <div className="login-container">
@@ -43,8 +54,11 @@ const Login = () => {
           value={pw}
           onChange={handlePwChanged}
         />
-        <button type="submit">로그인</button>
+        <button type="submit" disabled={isBtnDisabled}>
+          로그인
+        </button>
         <p onClick={() => navigate("/Signup")}>이메일로 회원가입</p>
+        {errorMesasge && <div className="error-message">{errorMesasge}</div>}
       </form>
     </div>
   );
---
+++ src/utils/emailValidation.js
@@ -0,0 +1,4 @@
+export const isValidEmail = (email) => {
+  const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  return emailRegex.test(email);
+}
