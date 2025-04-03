de30da38f0205a6f8a6f1c5693c4bdd1e25bbabe
gaeunchoi <gaeun1884@naver.com>
2025-04-03 20:54:11 +0900

    Feat: chatList Route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,15 @@ import {
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-
-const isAuthenticated = () => {
-  return !!localStorage.getItem("user");
-};
+import ChatList from "./pages/ChatList/ChatList";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={isAuthenticated() ? <Navigate to="./chatlist" /> : <Login />}
-        />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/chatlist"
-          element={isAuthenticated() ? <ChatList /> : <Navigate to="./login" />}
-        />
+        <Route path="/chatlist" element={<ChatList />} />
       </Routes>
     </Router>
   );
