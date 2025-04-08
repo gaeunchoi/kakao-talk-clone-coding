5f072c35f09ad4959d6bf1fc3e3c0b06c3ca20aa
gaeunchoi <gaeun1884@naver.com>
2025-04-08 21:32:44 +0900

    Fix: vercel 빌드 오류 - UserProfile import 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import "../reset.css";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import ChatList from "./pages/ChatList/ChatList";
-import UserProfile from "./pages/userProfile/userProfile";
+import UserProfile from "./pages/UserProfile/UserProfile";
+import MyChatRoom from "./pages/MyChatRoom/MyChatRoom";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/chatlist" element={<ChatList />} />
         <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/mychatroom" element={<MyChatRoom />} />
       </Routes>
     </Router>
   );
