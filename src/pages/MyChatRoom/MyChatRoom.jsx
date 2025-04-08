7033f7d31aadd2e3aa9a764d57e13a1b07659437
gaeunchoi <gaeun1884@naver.com>
2025-04-08 22:24:18 +0900

    Feat: MyChatRoom 초기 구조 생성

---
+++ src/pages/MyChatRoom/MyChatRoom.jsx
@@ -0,0 +1,11 @@
+import "./MyChatRoom.css";
+
+const MyChatRoom = () => {
+  return (
+    <div className="my-chat-room-container">
+      <h1>mychatroom</h1>
+    </div>
+  );
+};
+
+export default MyChatRoom;
