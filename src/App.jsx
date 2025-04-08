0bcf7e10a36037a9742b68afadefb4397f795003
gaeunchoi <gaeun1884@naver.com>
2025-04-08 20:21:58 +0900

    Design: reset.css 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "../reset.css";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import ChatList from "./pages/ChatList/ChatList";
+import UserProfile from "./pages/userProfile/userProfile";
 
 function App() {
   return (
@@ -16,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/chatlist" element={<ChatList />} />
+        <Route path="/userprofile" element={<UserProfile />} />
       </Routes>
     </Router>
   );
