88c584dd255899f42f1938b863743e73b80e971a
gaeunchoi <gaeun1884@naver.com>
2025-04-16 17:14:12 +0900

    Feat: 이름 수정 + 프로필 수정 취소 기능 추가

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -7,6 +7,7 @@ const UserProfile = () => {
   const user = JSON.parse(localStorage.getItem("loginUser"));
   const token = localStorage.getItem("token");
   const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(user.name || "");
   const [bio, setBio] = useState(user.bio || "");
   const navigate = useNavigate();
 
@@ -30,7 +31,7 @@ const UserProfile = () => {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
-          body: JSON.stringify({ bio }),
+          body: JSON.stringify({ name, bio }),
         }
       );
 
@@ -55,21 +56,41 @@ const UserProfile = () => {
             alt="내 프로필사진"
             className="profile-img"
           />
-          <h2>{user.name}</h2>
           {isEditing ? (
-            <input
-              type="text"
-              value={bio}
-              onChange={(e) => setBio(e.target.value)}
-              className="input-bio"
-            />
+            <>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="modify-input"
+              />
+              <input
+                type="text"
+                value={bio}
+                onChange={(e) => setBio(e.target.value)}
+                className="modify-input"
+              />
+            </>
           ) : (
-            <p>{user.bio || "상태메시지가 없습니다."}</p>
+            <>
+              <h2>{user.name}</h2>
+              <p>{user.bio || "상태메시지가 없습니다."}</p>
+            </>
           )}
         </div>
         <div className="user-profile-func">
           {isEditing ? (
             <>
+              <button
+                className="profile-btn"
+                onClick={() => {
+                  setIsEditing(false);
+                  setName(user.name || "");
+                  setBio(user.bio || "");
+                }}
+              >
+                취소
+              </button>
               <button className="profile-btn" onClick={handleSaveProfile}>
                 프로필 저장
               </button>
