ad276e138a3206b6c083bc65b72f3460671cdfb0
gaeunchoi <gaeun1884@naver.com>
2025-04-18 17:30:41 +0900

    Fix: Signup 비밀번호 에러메시지 수정

--- src/utils/pwValidation.js
+++ src/utils/pwValidation.js
@@ -2,22 +2,22 @@ export const isValidPassword = (pw, id) => {
   const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
   const minLength = 8;
 
-  if(pw.length < minLength || !specialCharRegex.test(pw)){
-    return{
+  if (pw.length < minLength || !specialCharRegex.test(pw)) {
+    return {
       valid: false,
       message: "비밀번호는 8자 이상이며, 특수문자를 포함해야합니다.",
     };
   }
 
-  for(let i = 0 ; i <= id.length - 4; i++){
-    const substring = id.slice(i, i+4);
-    if(pw.includes(substring)){
+  for (let i = 0; i <= id.length - 4; i++) {
+    const substring = id.slice(i, i + 4);
+    if (pw.includes(substring)) {
       return {
         valid: false,
-        message: "비밀번호에 아이디와 4글자 이상 연속된 부분이 포함되어 있습니다.",
+        message: "아이디와 4글자 이상 연속된 부분이 포함되어 있습니다.",
       };
     }
   }
 
-  return {valid: true, message: ""};
-}
+  return { valid: true, message: "" };
+};
