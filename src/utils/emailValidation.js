71e1884de318071ff35946aae2acab020ecd3e59
gaeunchoi <gaeun1884@naver.com>
2025-05-09 21:53:24 +0900

    Fix: 이메일 유효성 검사 -> 도메인 gTLD 자리수 제한 변경

--- src/utils/emailValidation.js
+++ src/utils/emailValidation.js
@@ -1,4 +1,5 @@
 export const isValidEmail = (email) => {
-  const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  const emailRegex =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-z]{2,}$/i;
   return emailRegex.test(email);
-}
+};
